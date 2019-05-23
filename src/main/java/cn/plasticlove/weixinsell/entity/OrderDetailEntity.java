ea58590f9b4d5370166e06d55f70a9310c4e13f5
luka <dgm_wjy0515@seu.edu.cn>
2019-05-23 23:03:14 +0800

    finish OrderDetailEntity

---
+++ src/main/java/cn/plasticlove/weixinsell/entity/OrderDetailEntity.java
@@ -0,0 +1,52 @@
+package cn.plasticlove.weixinsell.entity;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * The type Order detail entity.
+ *
+ * @author Luka
+ * @date 2019 /05/23
+ */
+@Data
+public class OrderDetailEntity {
+    /**
+     * order detail id
+     */
+    private String datailId;
+    /**
+     * order id
+     */
+    private String orderId;
+    /**
+     * product id
+     */
+    private String productId;
+    /**
+     * product name
+     */
+    private String productName;
+    /**
+     * product price
+     */
+    private BigDecimal productPrice;
+    /**
+     * product quantity
+     */
+    private Integer productQuantity;
+    /**
+     * product icon
+     */
+    private String productIcon;
+    /**
+     * create time
+     */
+    private Date createTime;
+    /**
+     * updateTime
+     */
+    private Date uodateTime;
+}
